42ef4223a7b91505bc9cb78c80ccf58e7f2cfc4d
julia-pro <juliaszatkowska52@gmail.com>
2023-11-06 10:40:03 +0100

    changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,22 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-    const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
-    const LOCAL_KEY = `videoplayer-current-time`;
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_KEY = `videoplayer-current-time`;
 
-    player.on('timeupdate', throttle(function({seconds}) {
-        localStorage.setItem(LOCAL_KEY, seconds);
+async function initPlayer() {
+  try {
+    await player.ready(); // Wait for the player to be ready
+    const time = localStorage.getItem(LOCAL_KEY) || 0;
+    player.setCurrentTime(time);
+
+    player.on('timeupdate', throttle(function({ seconds }) {
+      localStorage.setItem(LOCAL_KEY, seconds);
     }, 1000));
+  } catch (error) {
+    console.error('Error initializing player:', error);
+  }
+}
 
-        const time = localStorage.getItem(LOCAL_KEY) || 0;
-        player.setCurrentTime(time);
+initPlayer();
