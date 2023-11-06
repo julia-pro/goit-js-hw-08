f3353ee992a732b9afb465978f415c81e213dcc3
julia-pro <juliaszatkowska52@gmail.com>
2023-11-06 10:00:30 +0100

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const LOCAL_KEY = `videoplayer-current-time`;
+
+    player.on('timeupdate', throttle(function({seconds}) {
+        localStorage.setItem(LOCAL_KEY, seconds);
+    }, 1000));
+
+        const time = localStorage.getItem(LOCAL_KEY) || 0;
+        player.setCurrentTime(time);
